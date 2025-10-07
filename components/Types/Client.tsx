7e4238de8f6c528004aa50c27e8a3b08a1953089
Ni-aina <niaina.dev.soft@gmail.com>
2025-10-07 18:36:54 +0300

    Features/reusable modal component

--- components/Types/Client.tsx
+++ components/Types/Client.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import Navbar from "../Navbar";
 import Table from "../Table";
+import { Modal } from "../ui/modal";
 
 const ClientType = () => {
     const [search, setSearch] = useState("");
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const headers = ["Type", "Description"];
     const body = {
@@ -39,7 +41,8 @@ const ClientType = () => {
 
     const title = "Vehicle types"
 
-    const handleAddType = () => {
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
 
     }
 
@@ -49,13 +52,55 @@ const ClientType = () => {
                 title={title}
                 search={search}
                 setSearch={setSearch}
-                onAdd={handleAddType}
+                onAdd={() => setIsModalOpen(true)}
             />
             <Table
                 title={title}
                 headers={headers}
                 body={body}
             />
+            <Modal
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                title="Add vehicle type"
+            >
+                <form
+                    className="flex flex-col gap-3"
+                    onSubmit={handleSubmit}
+                >
+                    <div className="flex flex-col gap-3">
+                        <label htmlFor="type">Type</label>
+                        <input
+                            type="text"
+                            name="type"
+                            required
+                            className="outline-none px-4 py-2 border border-white/70 rounded-sm"
+                        />
+                        <label htmlFor="description">Description</label>
+                        <textarea
+                            name="description"
+                            required
+                            className="outline-none px-4 py-2 border border-white/70 rounded-sm"
+                        />
+                    </div>
+                    <div className="mt-3 w-full flex justify-end gap-3">
+                        <button
+                            type="button"
+                            onClick={() => setIsModalOpen(false)}
+                            className="w-[120px] h-[40px] flex justify-center items-center 
+                            bg-white/10 rounded-sm cursor-pointer hover:opacity-80"
+                        >
+                            Cancel
+                        </button>
+                        <button
+                            className="w-[120px] h-[40px] flex justify-center items-center 
+                            bg-blue-950/20 rounded-sm cursor-pointer hover:opacity-80"
+                        >
+                            Add
+                        </button>
+                    </div>
+                </form>
+            </Modal>
         </div>
     )
 }
---
+++ components/ui/modal.tsx
@@ -0,0 +1,46 @@
+import { XCircle } from "lucide-react";
+import { ReactNode } from "react";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title?: string;
+  children: ReactNode;
+}
+
+export const Modal = ({
+  isOpen,
+  onClose,
+  title,
+  children
+}: ModalProps) => {
+  if (!isOpen) return null;
+
+  return (
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center bg-white/10 backdrop-blur-sm"
+      onClick={onClose}
+    >
+      <div
+        className="bg-black/80 rounded-lg shadow-lg w-full max-w-md p-6 relative text-white/80"
+        onClick={(e) => e.stopPropagation()}
+      >
+        {
+          title &&
+          <h2 className="text-lg font-semibold mb-4">
+            {title}
+          </h2>
+        }
+        {children}
+        <button
+          className="absolute top-5 right-5 hover:opacity-80 cursor-pointer"
+          onClick={onClose}
+        >
+          <XCircle
+            size={24}
+          />
+        </button>
+      </div>
+    </div>
+  )
+}
