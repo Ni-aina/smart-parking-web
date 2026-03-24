9a135fa4b1cce3652f225956f4e990b9ceca671e
ni-aina <niaina.dev.soft@gmail.com>
2026-03-24 16:30:33 +0300

    Remove h-screen

--- app/owner/notifications/page.tsx
+++ app/owner/notifications/page.tsx
@@ -2,7 +2,7 @@ import NoData from "@/components/ui/noData";
 
 const NotificationsPage = () => {
     return (
-        <div className="h-full">
+        <div className="mt-5 lg:mt-10">
             <NoData 
                 message="No notification yet"
             />
